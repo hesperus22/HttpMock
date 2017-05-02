2db5e1028114f0e117aac26d4ded9da5dd17eacb
Przemko Robakowski <przemko@przemko-robakowski.pl>
2017-05-02 12:16:31 +0200

    Build change

--- buildpackage.ps1
+++ buildpackage.ps1
@@ -1,2 +1,2 @@
 .nuget\nuget restore
-& ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe HttpMock.sln /property:Configuration=Release
+& ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe HttpMock.sln /p:OutputPath=$PSScriptRoot/bin-debug /property:Configuration=Debug
